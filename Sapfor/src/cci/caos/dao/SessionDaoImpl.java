3c08a2939a18da80a8e2dbb5e27cadaa91711648
TheoPerrin <theoperrin@ymail.com>
2017-02-19 15:08:44 +0100

    SessionDaoImpl : creer(Session session)

--- Sapfor/src/cci/caos/dao/SessionDaoImpl.java
+++ Sapfor/src/cci/caos/dao/SessionDaoImpl.java
@@ -4,9 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 import cci.caos.repository.Candidature;
 import cci.caos.repository.Session;
+import cci.caos.repository.Uv;
 import cci.caos.server.SapforServer;
 
 import static cci.caos.dao.DaoUtilitaires.fermeturesSilencieuses;
@@ -14,7 +17,9 @@ import static cci.caos.dao.DaoUtilitaires.fermeturesSilencieuses;
 public class SessionDaoImpl implements SessionDao {
 	
 	private DaoFactory daoFactory;
-	private static final String SQL_SELECT_PAR_ID = "SELECT * FROM Uv WHERE idUv = ?";
+	private static final String SQL_SELECT_PAR_ID = "SELECT * FROM Session WHERE idUv = ?";
+	private static final String SQL_INSERT_SESSION = "INSERT INTO Session VALUES(?, ?, ?, ?, ?, ?);";
+
 	
 	/*Constructeur */
 	
@@ -26,8 +31,30 @@ public class SessionDaoImpl implements SessionDao {
 	
 	@Override
 	public void creer (Session session) throws DAOException{
-		//TODO
+		 int               id = session.getId();
+	     String            nom = session.getNom();
+	     Date              dateDebut = session.getDateDebut();
+	     Date 			   dateFin = session.getDateFin();
+	     boolean           ouverteInscription = session.isOuverteInscription();
+	     int               idUv = session.getUv().getId() ;
+	     int 			   statut;
+	     
+	     Connection connexion = null; 
+	     PreparedStatement preparedStatement = null; 
+	     
+	     try{
+	     connexion = daoFactory.getConnection();
+	     preparedStatement = DaoFactory.initialisationRequetePreparee(connexion, SQL_INSERT_SESSION, false, id, nom, dateDebut, dateFin, ouverteInscription, idUv);
+	     statut = preparedStatement.executeUpdate(); 
+	     if (statut==0){System.out.println("ok");};
+	     } catch ( SQLException e ) {
+         	throw new DAOException( e );
+			}
+			finally {
+				fermeturesSilencieuses( preparedStatement, connexion );
+			}
 	}
+	
 	
 	@Override
 	public Session trouver (int id) throws DAOException{
@@ -48,7 +75,6 @@ public class SessionDaoImpl implements SessionDao {
 				session.setNom(resultSet.getString("nom"));
 				session.setOuverteInscription(resultSet.getBoolean("ouverteInscription"));
 				session.setUv(SapforServer.getSessionServer().getUvById(resultSet.getInt("id")));
-				
 			}
 		}	catch ( SQLException e ) {
             	throw new DAOException( e );
@@ -56,17 +82,9 @@ public class SessionDaoImpl implements SessionDao {
 			finally {
 				fermeturesSilencieuses( resultSet, preparedStatement, connexion );
 			}
-		
-		
-		
 		return session;
 	}
 	
-	@Override
-	public Session ajouterCandidat(Candidature candidature) throws DAOException{
-		//TOOD
-		return null;
-	}
 	
 
 }
